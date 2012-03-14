ab80b30e5f40208ee027f8396a2b44681a156d1d
Peter Jin <peterhaijin@gmail.com>
2012-03-14 19:11:19 -0400

    Stylistic changes.

--- plot.py
+++ plot.py
@@ -9,17 +9,16 @@ import csv
 
 # http://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python
 def file_len(fname):
-  with open(fname) as f:
-    for i, l in enumerate(f):
-      pass
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
     return i + 1
-  return 0
 
 def day_format(d):
-  return lambda x, pos: date.fromtimestamp(86400 * (d + float(x))).strftime('%b %d')
+    return lambda x, pos: date.fromtimestamp(86400 * (d + float(x))).strftime('%b %d')
 
 def hour_format(h):
-  return lambda x, pos: int(x) / h
+    return lambda x, pos: int(x) / h
 
 log_iter = csv.reader(open('kernel.log.out', 'rb'), delimiter=' ')
 log_len = file_len('kernel.log.out')
@@ -28,9 +27,9 @@ day = np.zeros(log_len)
 hour = np.zeros(log_len)
 
 for i, row in enumerate(log_iter):
-  t = int(row[0])
-  day[i] = floor(float(t) / 86400)
-  hour[i] = float((t % 86400)) / 3600
+    t = int(row[0])
+    day[i] = floor(float(t) / 86400)
+    hour[i] = float((t % 86400)) / 3600
 
 ndays = day.max() - day.min() + 1
 hour_lev = 4
@@ -42,12 +41,9 @@ extent = [x[-1], x[0], y[0], y[-1]]
 
 fig = plt.figure()
 
-#ax = plt.subplot(211)
 ax = plt.subplot(111)
 ax.xaxis.set_major_formatter(plt.FuncFormatter(day_format(day.min())))
 ax.yaxis.set_major_formatter(plt.FuncFormatter(hour_format(hour_lev)))
 img = plt.imshow(H, interpolation='nearest', cmap='gray')
-
-#ax = plt.subplot(212)
 
 plt.show()
