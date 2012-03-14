76f49e2c787cf9013ecb8a7e482b500f84d851d3
Peter Jin <peterhaijin@gmail.com>
2012-03-14 18:39:24 -0400

    Added log parsing and plotting scripts.

---
+++ parse.awk
@@ -0,0 +1,41 @@
+#!/usr/bin/awk -f
+BEGIN{
+  FS = ":| +";
+  formatspec = "%b %d %T";
+
+  lastyear_cmd = "TZ=GMT date +%Y";
+  if ((lastyear_cmd | getline result) > 0) {
+    last_year = result-1;
+    #system("echo " lastyear " >&2");
+  }
+  close(lastyear_cmd);
+
+  today_cmd = "TZ=GMT date +%s";
+  if ((today_cmd | getline result) > 0) {
+    today_ts = result;
+  }
+  close(today_cmd);
+}
+{
+  # Logfile times are in Month Day Hour:Minute:Second format in the local
+  # timezone. We manually adjust the timezone to GMT when we call date so we
+  # get multiple of 86400 day timestamps.
+  timespec = $1 " " $2 " " $3 ":" $4 ":" $5;
+  
+  # We need to figure out what year the date refers to. If the data timestamp is
+  # greater than the current timestamp, we know it was from last year.
+  timespec_cmd = "TZ=GMT date -jf '" formatspec "' '" timespec "' +%s";
+  if ((timespec_cmd | getline result) > 0) {
+    timespec_ts = result;
+    if (timespec_ts > today_ts) {
+      timespec_lastyear_cmd = "TZ=GMT date -jf '" formatspec " %Y' '" timespec " " last_year "' +%s";
+      if ((timespec_lastyear_cmd | getline result) > 0) {
+        timespec_ts = result;
+      }
+      close(timespec_lastyear_cmd);
+    }
+  }
+  close(timespec_cmd);
+
+  print timespec_ts;
+}
---
+++ plot.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+from datetime import *
+import csv
+
+# http://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python
+def file_len(fname):
+  with open(fname) as f:
+    for i, l in enumerate(f):
+      pass
+    return i + 1
+  return 0
+
+def day_format(d):
+  return lambda x, pos: date.fromtimestamp(86400 * (d + float(x))).strftime('%b %d')
+
+def hour_format(h):
+  return lambda x, pos: int(x) / h
+
+log_iter = csv.reader(open('kernel.log.out', 'rb'), delimiter=' ')
+log_len = file_len('kernel.log.out')
+
+day = np.zeros(log_len)
+hour = np.zeros(log_len)
+
+for i, row in enumerate(log_iter):
+  t = int(row[0])
+  day[i] = floor(float(t) / 86400)
+  hour[i] = float((t % 86400)) / 3600
+
+ndays = day.max() - day.min() + 1
+hour_lev = 4
+
+H, x, y = np.histogram2d(day, hour, bins=(ndays, hour_lev * 24))
+H = np.minimum(H, 1)
+H = H.transpose()
+extent = [x[-1], x[0], y[0], y[-1]]
+
+fig = plt.figure()
+
+#ax = plt.subplot(211)
+ax = plt.subplot(111)
+ax.xaxis.set_major_formatter(plt.FuncFormatter(day_format(day.min())))
+ax.yaxis.set_major_formatter(plt.FuncFormatter(hour_format(hour_lev)))
+img = plt.imshow(H, interpolation='nearest', cmap='gray')
+
+#ax = plt.subplot(212)
+
+plt.show()
